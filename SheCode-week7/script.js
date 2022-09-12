cadcdcd07c2dc694a4f41b94171944ba45605654
rulalub <rulalub@hotmail.com>
2022-09-12 20:36:48 +0300

    added apiUrl

--- SheCode-week7/script.js
+++ SheCode-week7/script.js
@@ -1,5 +1,7 @@
 
 let apiKey=" aa93cf045af7ef6b96d7f0bf356e88c3"
 
+let apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=aa93cf045af7ef6b96d7f0bf356e88c3"
+
 
 let day = days [now.getDay()];
